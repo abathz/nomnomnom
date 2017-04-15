fe81be9b1e0b77e6f02e840c2bd12ac50c5319f7
Adli Fariz Bonaputra <abathz@gmail.com>
2017-04-15 20:22:10 +0700

    redesign (little bit)

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
           <a href="https://www.facebook.com/nomnomnom.id/" target="_blank"><img src="./src/assets/img/facebook-logo.png" width="42px" alt=""/></a>
         </div>
         <div className="col-12 footer-info text-center">
-          <p>© 2017 HMPSM. All Rights Reserved. Developed by <a href="https://www.linkedin.com/in/adlifariz/" target="_blank">Adli Fariz Bonaputra</a></p>
+          <p>© 2017 Nom Nom Nom. All Rights Reserved. Developed by <a href="https://www.linkedin.com/in/adlifariz/" target="_blank">Adli Fariz Bonaputra</a></p>
         </div>
       </div>
     );
--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
     return (
       <div className="row animated fadeInUp">
         <div className="col-lg-12 mt-3 text-center">
-          <Link to="/"><img src="./src/assets/img/nomnomnom_logo.png" width="150px" alt=""/></Link>
+          <Link to="/"><img src="./src/assets/img/nomnomnom_logo.png" width="180px" alt=""/></Link>
         </div>
         <div className="mx-auto">
           <div className="navbar">
